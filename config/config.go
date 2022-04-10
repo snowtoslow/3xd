47105830b6f7fabf0d2879191228232ceedf39e5
snowtoslow <snowtoslow@gmail.com>
2022-04-10 15:44:25 +0300

    fix ParseConfigFile function

--- config/config.go
+++ config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"3xd/internal"
 	"github.com/spf13/viper"
 	"path/filepath"
 	"strings"
@@ -10,21 +9,25 @@ import (
 //Config struct to store the future configs
 type Config struct {
 	AppName string `mapstructure:"app_name" json:"app_name" yaml:"app_name"`
-	MadeBy string  `mapstructure:"made_by" json:"made_by" yaml:"made_by"`
+	MadeBy  string `mapstructure:"made_by" json:"made_by" yaml:"made_by"`
 }
 
-func ParseConfigFile(path string)(Config , error){
-	viper.AddConfigPath(path)
-	if fileExtension := filepath.Ext(path); fileExtension != ""{
-		viper.SetConfigType(strings.Trim(fileExtension,"."))
+func ParseConfigFile(path string) (Config, error) {
+	viper.AddConfigPath(filepath.Dir(path))
+	if fileExtension := filepath.Ext(path); fileExtension != "" {
+		viper.SetConfigType(strings.Trim(fileExtension, "."))
 	}
-	viper.SetConfigName(internal.DefaultConfigName)
+	_, nameWithExtension := filepath.Split(path)
+	if name := strings.TrimSuffix(nameWithExtension, filepath.Ext(path)); name != "" {
+		viper.SetConfigName(name)
+	}
+
 	var cfg Config
-	if err := viper.ReadInConfig();err != nil {
+	if err := viper.ReadInConfig(); err != nil {
 		return cfg, err
 	}
 
-	if err := viper.Unmarshal(&cfg); err != nil{
+	if err := viper.Unmarshal(&cfg); err != nil {
 		return cfg, err
 	}
 	return cfg, nil
